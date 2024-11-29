29ba2e08e2c3282feb1dd002ace925d3bc9c8375
qt <g.com>
2024-11-29 23:38:35 +0800

    33

---
+++ aadog.PInvoke.LibFridaCore/MarshalExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aadog.PInvoke.LibFridaCore
+{
+    public class MarshalExt
+    {
+        public static unsafe string? ConvertLPErrorToString(GError* error)
+        {
+            if (error == null)
+            {
+                return null;
+            }
+            return Marshal.PtrToStringUTF8(new IntPtr(error->message));
+        }
+    }
+}
